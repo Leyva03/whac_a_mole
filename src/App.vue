784c001fd81304025e4dbfc60a3b66d2ab434036
Leyva03 <pauleyva2003@gmail.com>
2024-09-17 19:05:37 +0200

    whac_a_mole_v1

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <h1>Whac-a-Mole</h1>
+  <div style="display: flex; flex-wrap: wrap">
+    <Mole></Mole><Mole></Mole><Mole></Mole>
+    <Mole></Mole><Mole></Mole><Mole></Mole>
+  </div>
+</template>
+
+<script>
+import Mole from './components/Mole.vue';
+
+export default {
+  name: 'App',
+  components: {
+    Mole
+  }
+};
+</script>
+<style>
+  div{
+  display:inline-block;
+  border: solid black;}
+</style>
---
+++ src/components/Mole.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="moleHole" @click="hitMole"><img :src="currentImage"></div>
+
+</template>
+
+<script>
+import empty from "../empty.png" 
+import mole from "../mole.png"
+import ouch from "../ouch.png"
+export default {
+  name: 'Mole',
+  data(){
+    return{
+      currentImage: empty,
+      hit: false
+    }
+  },
+  methods:{
+    hitMole(){
+      if(this.currentImage==mole){
+        this.currentImage = ouch
+        this.hit = true
+      }
+    },
+    showMole(){
+      this.currentImage = mole
+      setTimeout(()=>{
+        if(!this.hit){
+          this.hideMole()
+        }
+        
+      }, Math.random() * 1000 + 500)
+    },
+    hideMole(){
+      this.currentImage = empty
+      setTimeout(()=>{
+        if(!this.hit){
+          this.showMole()
+        }
+      }, Math.random() * 1000 + 500)
+    }
+
+  },
+  mounted(){
+    this.showMole()
+  }
+  
+};
+</script>
